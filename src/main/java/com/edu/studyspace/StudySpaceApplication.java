ba98bd16f79c67d76f1172e32b670cd8d2fe222c
vlash <shupliakovvl@gmail.com>
2024-03-07 14:55:50 +0200

    init

---
+++ src/main/java/com/edu/studyspace/StudySpaceApplication.java
@@ -0,0 +1,13 @@
+package com.edu.studyspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/edu/studyspace/StudySpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.studyspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudySpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
